8152c86d47aae52c5af5e06a5c936a8848675089
Israel Silva <israelitalo2012@gmail.com>
2022-07-31 17:48:57 -0300

    UseCallback e useMemo inseridos no desafio

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
@@ -30,18 +30,23 @@ interface MovieProps {
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
   const [movies, setMovies] = useState<MovieProps[]>([]);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
 
-  useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
+  const handleClickButton = useCallback((id: number) => {
+    setSelectedGenreId(id);
   }, []);
 
-  useEffect(() => {
+  const genres = useMemo(() => {
+    const localGenres: GenreResponseProps[] = [];
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      localGenres.push(...response.data);
+    });
+
+    return localGenres;
+  }, []);
+
+  const getMoviesAndGenresWithSelectedGenre = useCallback(() => {
     api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
       setMovies(response.data);
     });
@@ -49,11 +54,11 @@ export function App() {
     api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
       setSelectedGenre(response.data);
     })
-  }, [selectedGenreId]);
+  }, [selectedGenreId, api]);
 
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
+  useEffect(() => {
+    getMoviesAndGenresWithSelectedGenre();
+  }, [selectedGenreId]);
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
